e489ab436b978b7c303d050879bf545a6bfe6087
Mayraiden <ninegatesopen@gmail.com>
2025-12-24 16:42:41 +0300

    feat: фиксим дальше

--- backendStrapi/src/middlewares/debug-proto.ts
+++ backendStrapi/src/middlewares/debug-proto.ts
@@ -1,12 +1,22 @@
 export default (config: any, { strapi }: any) => {
 	return async (ctx: any, next: any) => {
+		// Исправляем протокол на основе X-Forwarded-Proto для работы за reverse proxy
+		const forwardedProto = ctx.request.headers['x-forwarded-proto']
+		if (forwardedProto === 'https') {
+			ctx.request.protocol = 'https'
+			ctx.request.secure = true
+			// Также устанавливаем для app.proxy
+			ctx.app.proxy = true
+		}
+		
 		console.log('--- DEBUG PROTOCOL ---')
 		console.log('protocol:', ctx.request.protocol)
 		console.log('secure:', ctx.request.secure)
-		console.log('x-forwarded-proto:', ctx.request.headers['x-forwarded-proto'])
+		console.log('x-forwarded-proto:', forwardedProto)
 		console.log('host:', ctx.request.headers['host'])
 		console.log('x-forwarded-host:', ctx.request.headers['x-forwarded-host'])
 		console.log('----------------------')
+		
 		await next()
 	}
 }
